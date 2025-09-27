c985a9412ca61f5ca4f54a778cccb79eab3fe761
PeriodicallyZoneOut <gtvbeon@gmail.com>
2025-09-27 07:47:52 -0700

    MergedCommit

--- test.cpp
+++ test.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     srand(time(NULL));
     cout<<"Hello world!"<<endl;
-    cout<<"This is a test file for git - new branch"<<endl;
+    cout<<"This is a test file for git"<<endl;
     cout<<"Random number: "<<rand()%100<<endl;
+    cout<<"Merge the branch fr";
     return 0;
 }
